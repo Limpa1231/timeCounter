b2d17dbf531fd860f8d17b9adf40b3bd70350bc9
ZdenisZ <90981641+Limpa1231@users.noreply.github.com>
2023-01-04 22:30:15 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+
+const currentYear = new Date().getFullYear();
+
+const summerTime  = new Date(`July  1 ${currentYear} 00:00:00`);
+
+
+//Update countDownTime
+
+function updateCount(){
+    const currentTime = new Date();
+    const diff = summerTime - currentTime;
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60; 
+
+    console.log(d,h,m,s)
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;  
+    
+}
+
+setInterval(updateCount, 1000); 
+
+
+
+
